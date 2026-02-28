1f38543ec155863311680d3e40058e9a87a9c063
Ayoub Bleblo <ayup79319@gmail.com>
2026-03-01 00:30:08 +0200

    update mock testing in tests\test_client.py - add integration testing that make sure you project is working clearly

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,35 +1,77 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import pytest
+
 from zabbix_minimal.client import ZabbixClint
-from zabbix_minimal.config import ZABBIX_URL, ZABBIX_TOKEN
+from zabbix_minimal.models import Problem
 
 
-def test_zabbix_client_url():
+"""URL Validation Test"""
+
+
+def test_zabbix_client_invalid_url():
     with pytest.raises(ValueError):
-        ZabbixClint("ftp://invalid-url.com", ZABBIX_TOKEN)
+        ZabbixClint("ftp://invalid-url.com", "token", ["22"])
+
+
+"""get_current_problems returns Problem objects"""
 
 
 def test_get_current_problems():
-    client = ZabbixClint(ZABBIX_URL, ZABBIX_TOKEN)
+    client = ZabbixClint("http://example.com", "token", ["22"])
 
-    fake_response = [{
+    fake_problem = [{
         "eventid": "1",
         "name": "CPU High",
         "severity": "3",
+        "acknowledged": "0",
         "clock": "1234567890",
+        "hosts": [{
+            "hostid": "1",
+            "name": "Server1",
+            "status": "0"
+        }]
     }]
 
-    with patch.object(client, "_call", return_value=fake_response):
+    fake_event_hosts = [{
+        "eventid": "1",
+        "hosts": [{
+            "hostid": "1",
+            "name": "Server1",
+            "status": "0"
+        }]
+    }]
+
+    with patch.object(client, "_call") as mock_call:
+        mock_call.side_effect = [fake_problem, fake_event_hosts]
+
         problems = client.get_current_problems()
-        assert len(problems)
+
+        assert len(problems) == 1
+        assert isinstance(problems[0], Problem)
         assert problems[0].name == "CPU High"
+        assert problems[0].primary_host.name == "Server1"
+
+
+"""is_connected Success"""
 
 
 def test_is_connected_success():
-    client = ZabbixClint("http://10.10.10.10", ZABBIX_TOKEN)
-    with patch.object(client.session, "post")as mock_post:
-        mock_post.return_value.status_code = 200
-        mock_post.return_value.json.return_value = {"result": "6.0.0"}
+    client = ZabbixClint("http://10.10.10.10", "token", ["22"])
+
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"result": "6.0.0"}
+    mock_response.raise_for_status.return_value = None
+
+    with patch.object(client.session, "post", return_value=mock_response):
         assert client.is_connected() is True
 
-        print(mock_post.return_value.status_code)
+
+"""is_connected Failure"""
+
+
+def test_is_connected_failure():
+    client = ZabbixClint("http://10.10.10.10", "token", ["22"])
+
+    with patch.object(client.session, "post", side_effect=Exception("Connection error")):
+        assert client.is_connected() is False
